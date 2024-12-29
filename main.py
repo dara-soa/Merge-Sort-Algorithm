4513799f53c0b249ae7a659d54a6c862a7ad8fe3
Daraujo-Soares <144711354+dara-soa@users.noreply.github.com>
2024-12-29 08:43:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def merge_sort(array):
+    if len(array) <= 1:
+        return
     
     middle_point = len(array) // 2
     left_part = array[:middle_point]
@@ -19,3 +21,20 @@ def merge_sort(array):
             array[sorted_index] = right_part[right_array_index]
             right_array_index += 1
         sorted_index += 1
+
+    while left_array_index < len(left_part):
+        array[sorted_index] = left_part[left_array_index]
+        left_array_index += 1
+        sorted_index += 1
+    
+    while right_array_index < len(right_part):
+        array[sorted_index] = right_part[right_array_index]
+        right_array_index += 1
+        sorted_index += 1
+
+
+if __name__ == '__main__':
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    print('Unsorted array: ')
+    print(numbers)
+    merge_sort(numbers)
